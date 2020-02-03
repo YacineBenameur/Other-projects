96e6738aea3bae468c081bb8613f053bce3fd1da
YacineBenameur <41924979+YacineBenameur@users.noreply.github.com>
2020-02-03 18:14:36 +0100

    Update ts_feature_extractor.py

--- ts_feature_extractor.py
+++ ts_feature_extractor.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import holidays
+from sklearn.preprocessing import PolynomialFeatures
 
 class FeatureExtractor(object):
 
@@ -17,7 +18,7 @@ class FeatureExtractor(object):
         df = X_ds.to_dataframe()
         df['time'] = pd.to_datetime(X_ds['time'].values)
         df = df.iloc[X_ds.n_burn_in:,:]
-
+        
         df['month'] = [d.month for d in df['time']]
         df['day_of_week'] = [d.weekday() for d in df['time']]
         df['day_of_month'] = [d.day for d in df['time']]
@@ -33,5 +34,13 @@ class FeatureExtractor(object):
                 df.loc[date,'holiday'] = 1
 
 
+        X_df = pd.get_dummies(df[['day_of_week','month','day_of_month','holiday']].applymap(str))
+        X_df['year'] = df['year'] #year variable is not treated as a categorical variable, so it's not one-hot encoded
+
+        poly_transformer = PolynomialFeatures(degree=2, interaction_only=False,include_bias=True).fit(X_df.values)
         
-        return df[['day_of_week','month','day_of_month','holiday']].values
+        X_train = poly_transformer.transform(X_df.values)
+
+
+        
+        return X_train
