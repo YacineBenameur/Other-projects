4c5c34a479ed7e381b8b4d218fa36172998069ab
YacineBenameur <41924979+YacineBenameur@users.noreply.github.com>
2020-02-26 17:43:19 +0100

    Update ts_feature_extractor.py

--- ts_feature_extractor.py
+++ ts_feature_extractor.py
@@ -24,6 +24,7 @@ class FeatureExtractor(object):
         yesterday = np.roll(X_df.values, 1, axis=0)[valid_range]
         week_ago = np.roll(X_df.values, 6, axis=0)[valid_range]
         month_ago = np.roll(X_df.values, 30, axis=0)[valid_range]
+        month_ago_2 = np.roll(X_df.values, 31, axis=0)[valid_range]
         week_ago_2 = np.roll(X_df.values, 7, axis=0)[valid_range]
         year_ago = np.roll(X_df.values, 7 * 52 - 1, axis=0)[valid_range]
         day_of_week = np.tile(X_df.index.dayofweek.values, (X_df.shape[1], 1)).T[valid_range]
@@ -32,6 +33,7 @@ class FeatureExtractor(object):
         df = X_ds.to_dataframe()
         df['time'] = pd.to_datetime(X_ds['time'].values)
         df['day_of_month'] = [d.day for d in df['time']]
+        df['month'] = [d.month for d in df['time']]
         
         GH = holidays.France()
 
@@ -46,11 +48,13 @@ class FeatureExtractor(object):
             week_ago,
             week_ago_2,
             month_ago,
+            month_ago_2,
             yesterday,
             weekly_rolling_mean,
             yearly_rolling_mean,
             day_of_week,
             store_id,
             df['holiday'].values[valid_range].reshape((-1,1)),
-            df['day_of_month'].values[valid_range].reshape((-1,1))
+            df['day_of_month'].values[valid_range].reshape((-1,1)),
+             df['month'].values[valid_range].reshape((-1,1)),
         ])
