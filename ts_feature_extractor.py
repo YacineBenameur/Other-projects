4cec0d8f54cf7cf7e18e037afaa9076fcf07cb71
YacineBenameur <41924979+YacineBenameur@users.noreply.github.com>
2020-02-26 17:02:56 +0100

    Update ts_feature_extractor.py

--- ts_feature_extractor.py
+++ ts_feature_extractor.py
@@ -57,6 +57,6 @@ class FeatureExtractor(object):
             monthly_weekday_rolling_mean,
             day_of_week,
             store_id,
-            df['holiday'].values[valid_range].reshape((-1,1))
+            df['holiday'].values[valid_range].reshape((-1,1)),
             df['day_of_month'].values[valid_range].reshape((-1,1))
         ])
